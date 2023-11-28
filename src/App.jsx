3f610f13326e49b831d2f9f4a6851fcd2b57efa5
Jupegarnica <juan@geekshubs.com>
2023-11-28 09:49:45 +0100

    Refactor Todos component and add initial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,67 @@
 import { useState } from "react";
 import "./App.css";
 
-const todosInitial = JSON.parse(localStorage.getItem("todos") || "[]");
+// const initialState = JSON.parse(localStorage.getItem("todos") || "[]");
+
+const initialState = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Learn JSX",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Learn Hooks",
+    completed: false,
+  },
+];
 
 function Todos() {
-  const [todos, _setTodos] = useState(todosInitial);
+  const [todos, _setTodos] = useState(initialState);
   const setTodos = (todos) => {
     _setTodos(todos);
     localStorage.setItem("todos", JSON.stringify(todos));
-  }
+  };
   const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
 
   const completeTodo = (id) => {
     const newTodos = todos.map((todo) => {
+      console.log("todo", todo);
       if (todo.id === id) {
         return {
           id: todo.id,
           title: todo.title,
           completed: !todo.completed,
         };
+      } else {
+        return todo;
       }
-      return todo;
-    })
+    });
     setTodos(newTodos);
-  }
+  };
+
+  const handleKeyUp = (event) => {
+    console.log("onKeyUp", event);
+    if (event.key === "Enter") {
+      const newTodo = {
+        id: Date.now(),
+        title: event.target.value,
+        completed: false,
+      };
+      setTodos([newTodo, ...todos]);
+      event.target.value = "";
+    }
+  };
 
   const nodes = todos.map((todo) => (
     <div
       key={todo.id}
-      className={"todo " + (todo.completed ? "completed" : "active")}
+      className={todo.completed ? "completed" : "active"}
     >
       <p className="todo-text">{todo.title}</p>
       <div className="todo-actions">
@@ -43,24 +76,16 @@ function Todos() {
       <input
         type="text"
         placeholder="Add Todo"
-        onKeyUp={(event) => {
-          if (event.key === "Enter") {
-            const newTodo = {
-              id: Date.now(),
-              title: event.target.value,
-              completed: false,
-            };
-            setTodos([newTodo, ...todos]);
-            event.target.value = "";
-          }
-        }}
+        onKeyUp={handleKeyUp}
       />
       <section>
         {nodes}
       </section>
-      <pre>
+      {
+        /* <pre>
         {JSON.stringify(todos, null, 2)}
-      </pre>
+      </pre> */
+      }
     </div>
   );
 }
